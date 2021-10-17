e3b585079c000cb452a8f150509917a0447215b9
EasySocHacks <easy.soc.hacks@gmail.com>
2021-10-17 16:30:01 +0300

    Add e2e playwright's tests

--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -2,19 +2,19 @@ export default function Registration() {
     return (
         <div>
             <form method="post"  name="register" autoComplete="off" action={process.env.REACT_APP_SERVER_URL + "/register"}>
-                <div>
+                <div className="login-field">
                     <p>Login: </p>
                     <input type="text" name="login"/>
                 </div>
-                <div>
+                <div className="age-field">
                     <p>Age: </p>
                     <input type="text" name="age"/>
                 </div>
-                <div>
+                <div className="password-field">
                     <p>Password: </p>
                     <input type="password" name="password"/>
                 </div>
-                <div>
+                <div className="submit-field">
                     <input type="submit" value="Register"/>
                 </div>
             </form>
