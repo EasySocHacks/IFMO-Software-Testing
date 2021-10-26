10d60b729038baf402f8dd8fc94d1be6a2c66f83
EasySocHacks <easy.soc.hacks@gmail.com>
2021-10-26 08:03:08 +0300

    Add annotation

--- spring/src/main/kotlin/easy/soc/hacks/spring/repository/UserRepository.kt
+++ spring/src/main/kotlin/easy/soc/hacks/spring/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package easy.soc.hacks.spring.repository
 
 import easy.soc.hacks.spring.domain.User
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: MongoRepository<User, Long> {
     fun findAllByOrderByIdDesc(): List<User>
 }
