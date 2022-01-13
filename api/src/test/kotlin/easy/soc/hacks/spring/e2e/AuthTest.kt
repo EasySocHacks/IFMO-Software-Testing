9769aec470aba313a2596821c48c2c3d57383863
EasySocHacks <easy.soc.hacks@gmail.com>
2022-01-13 05:33:00 +0300

    Add allure support

--- api/src/test/kotlin/easy/soc/hacks/spring/e2e/AuthTest.kt
+++ api/src/test/kotlin/easy/soc/hacks/spring/e2e/AuthTest.kt
@@ -4,11 +4,21 @@ import com.codeborne.selenide.Condition.text
 import com.codeborne.selenide.Selectors.*
 import com.codeborne.selenide.Selenide.`$`
 import com.codeborne.selenide.Selenide.open
+import io.qameta.allure.Description
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.Story
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
+@Epic("e2e test")
+@Feature("Auth")
 @ExtendWith(SelenoidExtension::class)
 class AuthTest {
+    @Story("User register and login")
+    @DisplayName("User can Auth")
+    @Description("User can register and then login")
     @Test
     fun `user can auth`() {
         open("register")
